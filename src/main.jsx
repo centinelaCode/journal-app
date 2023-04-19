06788dab4ef038ec6ae3ac68a61d28318bee3f85
centinela1024tb <centinela1024tb@gmail.com>
2023-04-18 18:22:10 -0600

    [ADD] Configuración final para el router (se agerga el BrowserRoute en main) y en JournalApp se agrega el route principal <AppRouter />

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import './styles.css'
+
 import { JournalApp } from './JournalApp'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <JournalApp />
-  </React.StrictMode>,
+	<React.StrictMode>
+		<BrowserRouter>
+			<JournalApp />    
+		</BrowserRouter>
+	</React.StrictMode>,
 )
