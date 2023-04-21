72e9afe8f6d13d985b4fe1e07e40e8c8112d212a
centinela1024tb <centinela1024tb@gmail.com>
2023-04-20 18:02:58 -0600

    [ADD] Se implkementa el boton flotante para agregar notes

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,3 +1,6 @@
+import { IconButton } from '@mui/material'
+import { AddOutlined } from '@mui/icons-material'
+
 import { JournalLayout } from '../layout/JournalLayout'
 import { NoteView, NothingSelectedView } from '../views'
 
@@ -12,6 +15,20 @@ export const JournalPage = () => {
 
       {/* NoteView -> seleccionado */}
       <NoteView />
+
+      <IconButton
+        size='large'
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: 0.8 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }}/>
+      </IconButton>
 
     </JournalLayout>    
   )
