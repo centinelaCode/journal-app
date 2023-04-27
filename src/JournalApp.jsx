45caf5d4c87b34349322d80a8ef814339a5d1eed
centinela1024tb <centinela1024tb@gmail.com>
2023-04-26 18:18:57 -0600

    [ADD] Se agrega la configuración de firebese con variables de entorno de vite

--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -1,7 +1,11 @@
 import { AppRouter } from './router/AppRouter'
 import { AppTheme } from './theme'
+// import { firebaseConfig } from './firebase/config'
+
+
 
 export const JournalApp = () => {
+  // console.log(firebaseConfig)
   return (
    <>
       <AppTheme>
