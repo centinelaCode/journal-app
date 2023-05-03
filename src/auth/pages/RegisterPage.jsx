b5944af4d9cd415011e04e49d47d47ff1d44c2a6
centinela1024tb <centinela1024tb@gmail.com>
2023-05-03 15:51:00 -0600

    [ADD] Se implementa en nuestro custom hook validación dinamica de campos
    => enviamos a useForm formValidations el cual es un objeto que debe de incluir una por propiedad por campo del formulario
    => cada propiedada es una array, donde la primera posición es la función con la logica de la validación
    => la segunda posición es un string con el mensaje del error
    => en nuestro useForm en un useEffect vamos a ejecutar la función createValidators donde vamos a barrer el array formValidations
       para obtener de forma dinamica las propiedades con el agregado al nombre de la propiedad <name_propertie>Valid
    => El valor será null en caso de que pase la validación o si no pasa la validación la propiedda tendra el valor del mensaje de
       error
    => En registerPage usamos nuestro useForm para validar los campos

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Link as RouterLink } from 'react-router-dom'
 import { Button, Grid, Link, TextField, Typography } from '@mui/material'
 
@@ -5,18 +6,32 @@ import { AuthLayout } from '../layout/AuthLayout'
 import { useForm } from '../../hooks'
 
 const formDate = {
-  email: 'rvaldez@google.com',
-  password: '123456',
-  displayName: 'Raul Valdez'
+  email: '',
+  password: '',
+  displayName: ''
+}
+
+const formValidations = {
+  email: [ (value) => value.includes('@') , 'El correo debe tener una @'],
+  password: [ (value) => value.length >= 6, 'El password debe de tener almenos 6 caracteres'],
+  displayName: [ (value) => value.length >= 1, 'El nombre es obligatorio'],
 }
 
 export const RegisterPage = () => {
 
+  const [formSubmitted, setFormSubmitted] = useState(false)
+
   //? se define que info manejara el form
-  const { displayName, email, password, onInputChange, formState } = useForm(formDate)
+  const { 
+      formState, displayName, email, password, onInputChange, 
+      isFormValid, displayNameValid, emailValid, passwordValid,
+    } = useForm(formDate, formValidations)
+
+    
 
   const onSubmit = ( event ) => {
     event.preventDefault();
+    setFormSubmitted(true); // state para asber cuando hacemos submit
 
     console.log(formState)
   }
@@ -24,6 +39,8 @@ export const RegisterPage = () => {
   return (
     
     <AuthLayout title="Crear Cuenta">
+
+      <h1>FormValid: { isFormValid ? 'Valido' : 'Incorrecto' }</h1>
 
       <form onSubmit={ onSubmit }>
         <Grid container>
@@ -36,6 +53,8 @@ export const RegisterPage = () => {
                 name="displayName"
                 value={ displayName }
                 onChange={ onInputChange }
+                error={ !!displayNameValid && formSubmitted }
+                helperText={ displayNameValid }
               />
           </Grid>
 
@@ -48,6 +67,8 @@ export const RegisterPage = () => {
                 name="email"
                 value={ email }
                 onChange={ onInputChange }
+                error={ !!emailValid && formSubmitted }
+                helperText={ emailValid }
               />
           </Grid>
 
@@ -60,6 +81,8 @@ export const RegisterPage = () => {
                 name="password"
                 value={ password }
                 onChange={ onInputChange }
+                error={ !!passwordValid && formSubmitted }
+                helperText={ passwordValid }
               />
           </Grid>
 
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,8 +1,24 @@
-import { useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 
-export const useForm = ( initialForm = {} ) => {
+export const useForm = (initialForm = {}, formValidations = {}) => {
   
     const [ formState, setFormState ] = useState( initialForm );
+    const [formValidation, setformValidation] = useState({})
+
+    useEffect(() => {
+        createValidators();        
+    },[ formState ]);
+
+    const isFormValid = useMemo( () => {
+        
+        for ( const formValue of Object.keys( formValidation )) {
+            console.log('FormValue:',formValue)
+            console.log('FormValidation',formValidation)
+            if(formValidation[formValue] !== null) return false;
+        }
+        
+        return true;
+    }, [formValidation])
 
     const onInputChange = ({ target }) => {
         const { name, value } = target;
@@ -16,10 +32,27 @@ export const useForm = ( initialForm = {} ) => {
         setFormState( initialForm );
     }
 
+    const createValidators = () => {
+        const formCheckedValues = {};
+
+        for (const formField of Object.keys(formValidations)) {
+            // console.log(formField); // imprime las key del object: email, password, displayName
+            const [fn, errorMessage ] = formValidations[formField];
+           
+            formCheckedValues[`${ formField }Valid`] = fn( formState[formField] ) ? null : errorMessage;
+        }
+
+        setformValidation(formCheckedValues);
+        console.log(formCheckedValues)
+    }
+
     return {
         ...formState,
         formState,
         onInputChange,
         onResetForm,
+
+        ...formValidation,
+        isFormValid,
     }
 }
