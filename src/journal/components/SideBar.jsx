c8a85990613eacd583d4c167a3e709a9268fdac4
centinela1024tb <centinela1024tb@gmail.com>
2023-05-04 17:58:26 -0600

    [ADD] implementación para mostrar el nombre del user (displayname) en el sideBar

--- src/journal/components/SideBar.jsx
+++ src/journal/components/SideBar.jsx
@@ -1,51 +1,55 @@
 import { TurnedInNot } from '@mui/icons-material'
 import { Box, Divider, Drawer, List, ListItem, ListItemButton, ListItemIcon, Toolbar, Typography, Grid, ListItemText } from '@mui/material'
+import { useSelector } from 'react-redux'
 
 export const SideBar = ({ drawerWidth = 240 }) => {
-  return (
-    <Box
-      component='nav'
-      sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0} }}
-    >
 
-      <Drawer
-         variant='permanent'  // temporaly
-         open
-         sx={{             
-            display: { xs: 'block' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth }
-         }}
+   const { displayName } = useSelector( state => state.auth )
+
+   return (
+      <Box
+         component='nav'
+         sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0} }}
       >
-         <Toolbar>
-            <Typography variant='h6' noWrap component='div' >
-               Raul Valdez Piedra 
-            </Typography>
-         </Toolbar>
 
-         <Divider />
+         <Drawer
+            variant='permanent'  // temporaly
+            open
+            sx={{             
+               display: { xs: 'block' },
+               '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth }
+            }}
+         >
+            <Toolbar>
+               <Typography variant='h6' noWrap component='div' >
+                  { displayName } 
+               </Typography>
+            </Toolbar>
 
-         
+            <Divider />
 
-         <List>
-            {
-               ['Enero', 'Febrero', 'Marzo', 'Abril'].map(text => (
-                  <ListItem key={text} disablePadding>
-                     <ListItemButton>  {/* para que se pueda hacer click en el */}
-                        <ListItemIcon>
-                           <TurnedInNot />
-                        </ListItemIcon>
-                        <Grid container> 
-                           <ListItemText primary={ text } />
-                           <ListItemText secondary={ 'Lorem ipsum dolor sit amet consectetur elit.' } />
-                        </Grid>                        
-                     </ListItemButton>
-                  </ListItem>   
-               ))
-            }
-         </List>
+            
 
-      </Drawer>
+            <List>
+               {
+                  ['Enero', 'Febrero', 'Marzo', 'Abril'].map(text => (
+                     <ListItem key={text} disablePadding>
+                        <ListItemButton>  {/* para que se pueda hacer click en el */}
+                           <ListItemIcon>
+                              <TurnedInNot />
+                           </ListItemIcon>
+                           <Grid container> 
+                              <ListItemText primary={ text } />
+                              <ListItemText secondary={ 'Lorem ipsum dolor sit amet consectetur elit.' } />
+                           </Grid>                        
+                        </ListItemButton>
+                     </ListItem>   
+                  ))
+               }
+            </List>
 
-    </Box>
-  )
+         </Drawer>
+
+      </Box>
+   )
 }
