fee1984f957dd89548af1b2c748fe6040272ae8a
centinela1024tb <centinela1024tb@gmail.com>
2023-05-04 16:48:05 -0600

    [ADD] implementación de un loading el cual se mostrará al cargar la aplicación mientras status = checking

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,9 +1,19 @@
 import { Route, Routes } from 'react-router-dom'
 import { AuthRoutes } from '../auth/routes/AuthRoutes'
 import { JournalRoutes } from '../journal/routes/JournalRoutes'
+import { useSelector } from 'react-redux'
+import { CheckingAuth } from '../ui'
 
 
 export const AppRouter = () => {
+
+   const { status } = useSelector( state => state.auth );
+
+   // si el ststus es cheking entonces mostramos el loader pq hay que ver si estamos o no authenticados
+   if(status === 'checking') {
+      return <CheckingAuth />
+   }
+
    return (
       <Routes>
 
